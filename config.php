b7b3962f8085140861c1d29998f7524571d65545
trinhthinhhp <cuongthinh.tk@gmail.com>
2014-04-11 20:04:54 +0700

    NukeViet 4.1

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @Project NUKEVIET 4.x
+ * @Author VINADES.,JSC (contact@vinades.vn)
+ * @Copyright (C) 2014 VINADES.,JSC. All rights reserved
+ * @License GNU/GPL version 2 or any later version
+ * @Createdate Fri, 11 Apr 2014 11:53:43 GMT
+ */
+
+if ( ! defined( 'NV_MAINFILE' ) )
+{
+	die( 'Stop!!!' );
+}
+
+$db_config['dbhost'] = '127.0.0.1';
+$db_config['dbport'] = '';
+$db_config['dbname'] = 'nukeviet41';
+$db_config['dbsystem'] = 'nukeviet41';
+$db_config['dbuname'] = 'root';
+$db_config['dbpass'] = 'vertrigo';
+$db_config['dbtype'] = 'mysql';
+$db_config['collation'] = 'utf8_general_ci';
+$db_config['persistent'] = false;
+$db_config['prefix'] = 'nv4';
+
+$global_config['idsite'] = 0;
+$global_config['sitekey'] = 'e6a674351ecbd3b70ea4fc7ac5a6454f';// Do not change sitekey!
+
+?>
